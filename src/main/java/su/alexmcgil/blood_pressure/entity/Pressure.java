94feaff490a6dd970d145ef683d8f5ef01f8bbc1
Aleksandr Medvedev <alexmcgil@vivaldi.net>
2023-06-08 22:51:11 +0300

    feat: remove unnecessary user

--- src/main/java/su/alexmcgil/blood_pressure/entity/Pressure.java
+++ src/main/java/su/alexmcgil/blood_pressure/entity/Pressure.java
@@ -4,8 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.CascadeType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,8 +24,6 @@ public class Pressure {
     Short diastolic;
     Short pulse;
     LocalDateTime timePressure;
-    @ManyToOne(cascade = CascadeType.ALL)
-    User user;
 
     @Override
     public String toString() {
